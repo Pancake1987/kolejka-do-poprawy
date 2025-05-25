d5dacf0d36f90729f994ac7a7d5e6e98a80686e6
Kacper Zagozda <255424@edu.p.lodz.pl>
2025-05-25 20:48:11 +0200

    array.cpp

--- array.cpp
+++ array.cpp
@@ -1,18 +1,20 @@
+#include <stdio.h>
+
 #define STACK_SIZE 5
-int queue_table[STACK_SIZE] = { 0 };
+
+int queue_table[STACK_SIZE] = {0};
 int queue_index = 0;
 int queue_front = 0;
 int queue_back = 0;
 
 int isQueueEmpty(void)
 {
-    return (queue_index == 0) ? 1 : 0;
+    return (queue_index == 0);
 }
-
 
 int isQueueFull(void)
 {
-    return (queue_index == (STACK_SIZE)) ? 1 : 0;
+    return (queue_index == STACK_SIZE);
 }
 
 int Pop(void)
@@ -20,11 +22,11 @@ int Pop(void)
     if (!isQueueEmpty())
     {
         int val = queue_table[queue_front];
-        queue_front = (queue_front + 1) % queue_table;
+        queue_front = (queue_front + 1) % STACK_SIZE;
         queue_index--;
         return val;
     }
-    return -1;
+    return -1; // Error: queue empty
 }
 
 int Top(void)
@@ -33,8 +35,7 @@ int Top(void)
     {
         return queue_table[queue_front];
     }
-    else
-
+    return -1; // Error: queue empty
 }
 
 void Push(int val)
@@ -45,5 +46,8 @@ void Push(int val)
         queue_back = (queue_back + 1) % STACK_SIZE;
         queue_index++;
     }
-
+    else
+    {
+        printf("Queue is full!\n");
+    }
 }
